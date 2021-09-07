17b7a502c0820dae510a39fb1f3d86bca8f0e1e3
Jesse <j.tom.benjamin@gmail.com>
2021-09-07 10:26:38 +1000

    Add focus lock to menu (still needs more work to be properly accessible)

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,98 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import FocusLock from "./FocusLock";
 
 const MobileMenu = () => {
 	const [isShowing, setIsShowing] = useState(false);
+
 	return (
 		<>
-			{isShowing && (
-				<div id="curtain-menu" style="width: 100vw; visibility: visible;">
-					<div class="curtain-button-placer">
-						<button
-							id="close-curtain-button"
-							class="curtain-menu-button"
-							aria-label="close navigation menu"
-							onClick={() => setIsShowing(!isShowing)}
-							style="opacity: 1;"
-						>
-							<svg
-								stroke="currentColor"
-								fill="none"
-								stroke-width="2"
-								viewBox="0 0 24 24"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								height="1.5rem"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<line x1="18" y1="6" x2="6" y2="18"></line>
-								<line x1="6" y1="6" x2="18" y2="18"></line>
-							</svg>
-						</button>
-					</div>
-					<div id="curtain-menu-container" style="opacity: 1;">
-						<nav>
-							<div class="menu-primary_navigation-container">
-								<ul id="menu-primary_navigation" class="nav">
-									<li
-										id="menu-item-157"
-										class="menu-item menu-item-type-post_type menu-item-object-post menu-item-157"
-									>
-										<a href="https://flow.kingsdesign.info/guys-by-the-truck/">
-											Guys by the Truck
-										</a>
-									</li>
-									<li
-										id="menu-item-162"
-										class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-162"
-									>
-										<a href="#">Dropdown</a>
-										<ul class="sub-menu">
-											<li
-												id="menu-item-158"
-												class="menu-item menu-item-type-post_type menu-item-object-post menu-item-158"
-											>
-												<a href="https://flow.kingsdesign.info/just-some-boots/">
-													Just Some Boots
-												</a>
-											</li>
-											<li
-												id="menu-item-159"
-												class="menu-item menu-item-type-post_type menu-item-object-post menu-item-159"
-											>
-												<a href="https://flow.kingsdesign.info/guy-in-a-tree/">
-													Guy in a Tree
-												</a>
-											</li>
-											<li
-												id="menu-item-160"
-												class="menu-item menu-item-type-post_type menu-item-object-post menu-item-160"
-											>
-												<a href="https://flow.kingsdesign.info/the-cat/">
-													The Cat
-												</a>
-											</li>
-											<li
-												id="menu-item-161"
-												class="menu-item menu-item-type-post_type menu-item-object-post menu-item-161"
-											>
-												<a href="https://flow.kingsdesign.info/tasmanian-trees/">
-													Tasmanian Trees
-												</a>
-											</li>
-										</ul>
-									</li>
-									<li
-										id="menu-item-354"
-										class="menu-item menu-item-type-post_type menu-item-object-page menu-item-354"
-									>
-										<a href="https://flow.kingsdesign.info/ffff/">ffff</a>
-									</li>
-								</ul>
-							</div>{" "}
-						</nav>
-					</div>
-				</div>
-			)}
 			<button
 				id="open-curtain-button"
 				aria-label="open navigation menu"
@@ -112,6 +25,101 @@ const MobileMenu = () => {
 					</g>
 				</svg>
 			</button>
+			{isShowing && (
+				<FocusLock>
+					<div
+						// ref={closeButton}
+						id="curtain-menu"
+						style="width: 100vw; visibility: visible;"
+					>
+						<div class="curtain-button-placer">
+							<button
+								id="close-curtain-button"
+								class="curtain-menu-button"
+								aria-label="close navigation menu"
+								onClick={() => setIsShowing(!isShowing)}
+								style="opacity: 1;"
+							>
+								<svg
+									stroke="currentColor"
+									fill="none"
+									stroke-width="2"
+									viewBox="0 0 24 24"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									height="1.5rem"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<line x1="18" y1="6" x2="6" y2="18"></line>
+									<line x1="6" y1="6" x2="18" y2="18"></line>
+								</svg>
+							</button>
+						</div>
+						<div id="curtain-menu-container" style="opacity: 1;">
+							<nav>
+								<div class="menu-primary_navigation-container">
+									<ul id="menu-primary_navigation" class="nav">
+										<li
+											id="menu-item-157"
+											class="menu-item menu-item-type-post_type menu-item-object-post menu-item-157"
+										>
+											<a href="https://flow.kingsdesign.info/guys-by-the-truck/">
+												Guys by the Truck
+											</a>
+										</li>
+										<li
+											id="menu-item-162"
+											class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-162"
+										>
+											<a href="#">Dropdown</a>
+											<ul class="sub-menu">
+												<li
+													id="menu-item-158"
+													class="menu-item menu-item-type-post_type menu-item-object-post menu-item-158"
+												>
+													<a href="https://flow.kingsdesign.info/just-some-boots/">
+														Just Some Boots
+													</a>
+												</li>
+												<li
+													id="menu-item-159"
+													class="menu-item menu-item-type-post_type menu-item-object-post menu-item-159"
+												>
+													<a href="https://flow.kingsdesign.info/guy-in-a-tree/">
+														Guy in a Tree
+													</a>
+												</li>
+												<li
+													id="menu-item-160"
+													class="menu-item menu-item-type-post_type menu-item-object-post menu-item-160"
+												>
+													<a href="https://flow.kingsdesign.info/the-cat/">
+														The Cat
+													</a>
+												</li>
+												<li
+													id="menu-item-161"
+													class="menu-item menu-item-type-post_type menu-item-object-post menu-item-161"
+												>
+													<a href="https://flow.kingsdesign.info/tasmanian-trees/">
+														Tasmanian Trees
+													</a>
+												</li>
+											</ul>
+										</li>
+										<li
+											id="menu-item-354"
+											class="menu-item menu-item-type-post_type menu-item-object-page menu-item-354"
+										>
+											<a href="https://flow.kingsdesign.info/ffff/">ffff</a>
+										</li>
+									</ul>
+								</div>{" "}
+							</nav>
+						</div>
+					</div>
+				</FocusLock>
+			)}
 		</>
 	);
 };
